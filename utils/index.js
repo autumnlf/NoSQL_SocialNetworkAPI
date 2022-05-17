4e67d1c72650ffc487c5661db3315796671b9937
autumnlf <autumnF12@gmail.com>
2022-05-17 14:45:07 -0400

    removed time format helper. Not necessary

--- utils/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    format_date: (date) => {
-        // Format date as MM/DD/YYYY HH:MM
-        const options = { month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' };
-        return date.toLocaleDateString(undefined, options);
-    }
-};
